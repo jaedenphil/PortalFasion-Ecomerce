8e55209387660ec86186a58c1f048c3204ec061f
Jaeden <n01538412@humber.ca>
2023-11-09 23:32:53 -0500

    PortalFashion-app-start

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <header>
+        {' '}
+        <a href="/">PortalFashion</a>
+      </header>
+
+      <main>List Products:</main>
+    </div>
+  );
+}
+
+export default App;
