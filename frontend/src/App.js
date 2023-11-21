2410992a7f257f21e3a87f972bc19c270f227284
Jaeden <n01538412@humber.ca>
2023-11-21 12:37:01 -0500

    MongoDB-Mongoose-Setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ import Nav from 'react-bootstrap/Nav';
 import { useContext } from 'react';
 import { Store } from './Store';
 import CartScreen from './screens/CartScreen';
-
+import SignInScreen from './screens/SignInScreen';
 function App() {
   const { state } = useContext(Store);
   const { cart } = state;
@@ -41,6 +41,7 @@ function App() {
             <Routes>
               <Route path="/product/:slug" element={<ProductScreen />} />
               <Route path="/cart" element={<CartScreen />} />
+              <Route path="/signin" element={<SignInScreen />} />
               <Route path="/" element={<HomeScreen />} />
             </Routes>
           </Container>
