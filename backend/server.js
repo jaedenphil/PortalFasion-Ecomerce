02918664c37767416ff3eceaad8a10a58e9ffcb7
Jaeden <n01538412@humber.ca>
2023-12-04 17:09:30 -0500

    serer.js-change

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,24 @@
 import express from 'express';
+import data from './data.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import seedRouter from './routes/seedRoutes.js';
-import productRouter from './routes/productRoutes.js';
-
 dotenv.config();
 
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
-    console.log('Connected to MongoDB');
+    console.log('Connected to MongoDb');
   })
   .catch((err) => {
     console.log(err.message);
   });
-
 const app = express();
-
-// Middleware for CORS
+const port = 3001;
+app.use(express.static('public'));
+// Root route
+app.get('/', (req, res) => {
+  res.send('Server is running on http://localhost:' + port);
+});
 app.use(function (req, res, next) {
   res.header(
     'Access-Control-Allow-Origin',
@@ -30,16 +31,29 @@ app.use(function (req, res, next) {
   next();
 });
 
-// Routes
-app.use('/api/seed', seedRouter);
-app.use('/api/products', productRouter);
-
-// Root route
-app.get('/', (req, res) => {
-  res.send('Server is running');
+// Products route
+app.get('/api/products', (req, res) => {
+  res.json(data.products);
 });
 
-const port = process.env.PORT || 5000;
+app.get('/api/products/slug/:slug', (req, res) => {
+  const product = data.products.find((x) => x.slug === req.params.slug);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).json({ message: 'Product Not Found' });
+  }
+});
+app.get('/api/products/:_id', (req, res) => {
+  const product = data.products.find((x) => x._id === req.params._id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product Not Found' });
+  }
+});
+
+// Start the server
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log('Server is running on http://localhost:' + port);
 });
