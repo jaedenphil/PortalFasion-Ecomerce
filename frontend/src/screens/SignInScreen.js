a5a81fed61252ae082be16ea666bf20bdd17aff8
Jaeden <n01538412@humber.ca>
2023-12-11 22:23:20 -0500

    t4

--- frontend/src/screens/SignInScreen.js
+++ frontend/src/screens/SignInScreen.js
@@ -3,7 +3,6 @@ import { Link, useLocation, useNavigate } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import { Helmet } from 'react-helmet-async';
 import { useContext, useEffect, useState } from 'react';
 import { Store } from '../Store';
 import { toast } from 'react-toastify';
@@ -47,9 +46,7 @@ export default function SignInScreen() {
 
   return (
     <Container className="small-container">
-      <Helmet>
-        <title>Sign In</title>
-      </Helmet>
+      <title>Sign In</title>
       <h1 className="my-3">Sign In</h1>
       <Form onSubmit={submitHandler}>
         <Form.Group className="mb-3" controlId="email">
